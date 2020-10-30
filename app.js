4546e0acd0fbe615f0277bc0060e912b70b18054
5wisdom <jihye9099@naver.com>
2020-10-30 22:19:27 +0900

    board connect

--- app.js
+++ app.js
@@ -22,9 +22,28 @@ app.locals.pretty = true;
 // middleware req가 지나가면서 json형식으로 변경해줌
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use('/', express.static(path.join(__dirname, './public')));
+
 
 //routers 라우터세팅
 app.use('/', express.static(path.join(__dirname, './public')));
 app.use('/board', boardRouter);
 app.use('/gallery', galleryRouter);
+
+//예외처리 error
+//404에러
+app.use((req, res, next) => {
+  const err = new Error();
+  err.code = 404;
+  err.msg = '요청하신 페이지를 찾을 수 없습니다.';
+  next(err);
+});
+
+//모든에러는 여기로 수렴됨
+//nest(err)은 여기로 받음 
+//500에러
+app.use((err, req, res, next) => {
+  console.log(err); //enoent는 pug에러로 console로 err 잡을수 있음
+  const code = err.code || 500;
+  const msg = err.msg || '서버 내부 오류입니다. 관리자에게 문의하세요.'
+  res.render('./error.pug', {code, msg});
+});
